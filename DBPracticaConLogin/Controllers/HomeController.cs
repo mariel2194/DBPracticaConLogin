13971225e857edfc8dddee14192bb881de5674aa
Scarlet Gonzalez <smgonzalez@neural.com.do>
2022-11-03 22:35:39 -0400

    Authorize tag

--- DBPracticaConLogin/Controllers/HomeController.cs
+++ DBPracticaConLogin/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace DBPracticaConLoginSearchYList.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
