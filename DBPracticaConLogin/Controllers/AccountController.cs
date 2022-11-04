073b27b4ba27cc36bffa9724d01995c8936da4d8
Scarlet Gonzalez <smgonzalez@neural.com.do>
2022-11-03 22:30:27 -0400

    Loog ooff

--- DBPracticaConLogin/Controllers/AccountController.cs
+++ DBPracticaConLogin/Controllers/AccountController.cs
@@ -397,7 +397,7 @@ namespace DBPracticaConLoginSearchYList.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
